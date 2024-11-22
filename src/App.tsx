4da0fa6c00cb46bb041ab87de2f9babe37c722e9
Alex Qin <qianjin.qin@qq.com>
2024-11-22 21:37:38 +0800

    add the progress bar

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import './App.css';
 import Search from './components/Search';
 
 import { Button } from '@mui/material';
+import LinearProgress from '@mui/material/LinearProgress';
+import Box from '@mui/material/Box';
 import { cn } from './lib/utils';
 
 function App() {
@@ -32,6 +34,15 @@ function App() {
       <Button color='primary'>Primary</Button>
       <Button color='secondary'>Secondary</Button>
       <h1 className={cn('text-3xl font-bold underline text-red-600', count === 2 && 'text-blue-500')}>Hello world!</h1>
+      <Box>
+        <LinearProgress variant='determinate' color='secondary' value={30} />
+        <LinearProgress variant='determinate' color='success' value={30} />
+        <LinearProgress variant='determinate' color='inherit' value={40} />
+      </Box>
+      <div className='relative mt-10'>
+        <LinearProgress variant='determinate' value={70} className='h-4' />
+        <LinearProgress variant='determinate' value={30} className='h-4' sx={{ position: 'absolute', top: 0, left: 0, width: '100%' }} />
+      </div>
     </>
   );
 }
